ea85cd97925c1c8bc58f1b2efec9452d5982aee9
mariacordero <maria.cordero6@upr.edu>
2018-12-01 08:53:04 -0400

    Adding calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { routes } from '../app/app.router';
 import { FusionChartsModule } from 'angular-fusioncharts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+// import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+// import { DemoComponent } from '.component';
+
 
 // Load FusionCharts
 import * as FusionCharts from 'fusioncharts';
@@ -36,7 +45,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     LeaderboardComponent,
     ScrambleReviewComponent,
     ScramblePlayComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +54,15 @@ import { ProfileComponent } from './components/profile/profile.component';
     NgbModule,
     FusionChartsModule,
     routes,
-  
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    // FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,7 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  dataSource: Object;
+  chartConfig: Object;
+
+  constructor() { 
+    this.chartConfig = {
+      width: '700',
+      height: '400',
+      type: 'column2d',
+      dataFormat: 'json',
+    };
+
+    this.dataSource = {
+      "chart": {
+        "caption": "Weekly Report:",
+        "xAxisName": "Assigments Completed",
+        "yAxisName": "Days",
+        "theme": "fusion",
+      },
+      "data": [{
+        "label": "Sunday",
+        "value": "3"
+      }, {
+        "label": "Monday",
+        "value": "5"
+      }, {
+        "label": "Tuesday",
+        "value": "13"
+      }, {
+        "label": "Wednesday",
+        "value": "15"
+      }, {
+        "label": "Thursday",
+        "value": "11"
+      }, {
+        "label": "Friday",
+        "value": "0"
+      }, {
+        "label": "Saturday",
+        "value": "2"
+      }]
+    };
+  }
 
   ngOnInit() {
   }
