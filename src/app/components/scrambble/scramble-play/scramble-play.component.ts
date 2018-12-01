1338dd7e845c5ee8614855e2665f0535aaf9d6cf
Gabriel Huertas <gabriel.huertas1@upr.edu>
2018-12-01 08:51:03 -0400

    Merge with julio_game

--- src/app/components/scrambble/scramble-play/scramble-play.component.ts
+++ src/app/components/scrambble/scramble-play/scramble-play.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-scramble-play',
@@ -11,5 +12,18 @@ export class ScramblePlayComponent implements OnInit {
 
   ngOnInit() {
   }
+  review_words = [
+    {word: "vinegar", definition: 'dilute acetic acid', type: 'noun'},
+    {word: "ball", definition: 'a lavish dance requiring formal attire', type: "noun"},
+    {word: "violet", definition: 'of a color intermediate between red and blue', type: "adjective"},
+    {word: "spring", definition: 'the elasticity of something that can be stretched and returns to its original length', type: "noun"},
+    {word: "masterpiece", definition: 'the most outstanding work of a creative artist or craftsman', type: "noun"},
+    {word: "dig", definition: 'turn up, loosen, or remove earth', type: "verb"}
+  ];
 
+  scramble=function(str:string) {
+    var split_word = str.split("");
+    split_word.forEach(function (value));
+    return split_word;
+  }
 }
--- src/app/components/scrambble/scramble-review/scramble-review.component.ts
+++ src/app/components/scrambble/scramble-review/scramble-review.component.ts
@@ -16,11 +16,7 @@ export class ScrambleReviewComponent implements OnInit {
     {word: "ball", definition: 'a lavish dance requiring formal attire', type: "noun"},
     {word: "violet", definition: 'of a color intermediate between red and blue', type: "adjective"},
     {word: "spring", definition: 'the elasticity of something that can be stretched and returns to its original length', type: "noun"},
-    {word: "masterpiece", definition: 'the most outstanding work of a creative artist or craftsman', type: "noun"}
-    // {word: 6, definition: 'Carbon', type: 12.0107},
-    // {word: 7, definition: 'Nitrogen', type: 14.0067},
-    // {word: 8, definition: 'Oxygen', type: 15.9994},
-    // {word: 9, definition: 'Fluorine', type: 18.9984},
-    // {word: 10, definition: 'Neon', type: 20.1797},
+    {word: "masterpiece", definition: 'the most outstanding work of a creative artist or craftsman', type: "noun"},
+    {word: "dig", definition: 'turn up, loosen, or remove earth', type: "verb"}
   ];
 }
