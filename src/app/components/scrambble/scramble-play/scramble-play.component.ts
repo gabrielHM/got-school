32dcb7d8aa3efa54b8466f78cb8c883979f23d24
Julio Aguilar <julio.aguilar@upr.edu>
2018-12-01 05:14:09 -0400

    Added game review and game play to scrambble

--- src/app/components/scrambble/scramble-play/scramble-play.component.ts
+++ src/app/components/scrambble/scramble-play/scramble-play.component.ts
@@ -11,5 +11,12 @@ export class ScramblePlayComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  review_words = [
+    {word: "vinegar", definition: 'dilute acetic acid', type: 'noun'},
+    {word: "ball", definition: 'a lavish dance requiring formal attire', type: "noun"},
+    {word: "violet", definition: 'of a color intermediate between red and blue', type: "adjective"},
+    {word: "spring", definition: 'the elasticity of something that can be stretched and returns to its original length', type: "noun"},
+    {word: "masterpiece", definition: 'the most outstanding work of a creative artist or craftsman', type: "noun"},
+    {word: "dig", definition: 'turn up, loosen, or remove earth', type: "verb"}
+  ];
 }
