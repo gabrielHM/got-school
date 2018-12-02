abd9fe5574eed542670660ddb5836ab22863446a
Gabriel Huertas <gabriel.huertas1@upr.edu>
2018-12-02 02:09:05 -0400

    Added victory gif

--- src/app/components/pvpmath/pvpmath.component.ts
+++ src/app/components/pvpmath/pvpmath.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MathService } from 'src/app/core/http/math.service'
+import { MathService } from 'src/app/core/http/math.service';
+// var mathmlToSvg = require("mathml-to-svg")
 
 @Component({
   selector: 'app-pvpmath',
@@ -8,12 +9,13 @@ import { MathService } from 'src/app/core/http/math.service'
 })
 export class PvpmathComponent implements OnInit {
   
-  mathP = {};
-  exercise = {"id":"yhbwbrct","question":"<mn>17<\/mn><mo> - <\/mo><mo> ( <\/mo><mn>13<\/mn><mo> + <\/mo><mn>25<\/mn><mo> ) <\/mo>","choices":["<math><mo>-<\/mo><mn>21<\/mn><\/math>","<math><mo>-<\/mo><mn>4<\/mn><\/math>","<math><mn>20<\/mn><\/math>","<math><mo>-<\/mo><mn>8<\/mn><\/math>","<math><mo>-<\/mo><mn>14<\/mn><\/math>"],"correct_choice":0,"instruction":"Evaluate","category":"Arithmetic","topic":"Simple","difficulty":"Intermediate"};
-  constructor(private mathService: MathService ) { }
+  mathP = "";
+  exercise = {"id":"yhbwbrct","question":"<math><mn>17</mn><mo> - </mo><mo> ( </mo><mn>13</mn><mo> + </mo><mn>25</mn><mo> ) </mo></math>","choices":["<math><mo>-<\/mo><mn>21<\/mn><\/math>","<math><mo>-<\/mo><mn>4<\/mn><\/math>","<math><mn>20<\/mn><\/math>","<math><mo>-<\/mo><mn>8<\/mn><\/math>","<math><mo>-<\/mo><mn>14<\/mn><\/math>"],"correct_choice":0,"instruction":"Evaluate","category":"Arithmetic","topic":"Simple","difficulty":"Intermediate"};
+  constructor(private mathService: MathService) { }
 
   ngOnInit() {
     // this.prepareQuestion();
+// this.mathP = mathmlToSvg(this.exercise.question);
   }
 
   prepareQuestion = function (){
